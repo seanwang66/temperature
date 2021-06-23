ffaa2b6524fb21a249c50080f6f8e2c0c724db7d
Sean Wang <seanwang66@hotmail.com>
2021-06-24 00:49:30 +0800

    Initial Commit

---
+++ temperature/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  temperature
+//
+//  Created by Sean on 2021/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
